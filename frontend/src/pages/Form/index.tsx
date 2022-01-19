d3ddb2517e232600ba1a5c4264e5b7f4bacb6a04
Felipe-Silva93 <felipesilva4127@gmail.com>
2022-01-18 21:18:49 -0300

    Props

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,15 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import './styles.css';
 
+type props ={
+    movie:Movie;
+}
 function Form() {
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+    const params = useParams();
 
 
     return (
